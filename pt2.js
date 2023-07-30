12b4bb079582dad24e1dfebbe72f5aded1cd8dd2
shakibnymr <shakibnymr11@gmail.com>
2023-07-31 02:45:30 +0600

    edited

--- pt2.js
+++ pt2.js
@@ -3,23 +3,145 @@ var tomNum = 66;
 var janeNum = 95;
 var peteNum = 56;
 var johnNum = 40;
-if(myNum >= 80 && janeNum >= 80) {
-    console.log('A+')
+// myNum
+if (80 <= myNum) {
+    console.log('A grade');
 }
-if(tomNum >= 60 ) {
-    console.log('B')
+else if (60 <= myNum) {
+    console.log('B grade')
+}
+else if (50 <= myNum) {
+console.log('C grade')
+}
+else if (40 <= myNum) {
+console.log('D grade')
+}
+else if (39 >= myNum) {
+console.log('F grade')
 }
 else {
-    console.log('C')
+console.log('F grade')
 }
-if(peteNum >= 50) {
-    console.log('C')
+// tom grade
+if (80 <= tomNum) {
+    console.log('A grade');
 }
-if(johnNum >= 40) {
-    console.log('D')
+else if (60 <= tomNum) {
+    console.log('B grade')
+}
+else if (50 <= tomNum) {
+console.log('C grade')
+}
+else if (40 <= tomNum) {
+console.log('D grade')
+}
+else if (39 >= tomNum) {
+console.log('F grade')
+}
+else {
+console.log('F grade')
+}
+// jane
+if (80 <= janeNum) {
+    console.log('A grade');
+}
+else if (60 <= janeNum) {
+    console.log('B grade')
+}
+else if (50 <= janeNum) {
+console.log('C grade')
+}
+else if (40 <= janeNum) {
+console.log('D grade')
+}
+else if (39 >= janeNum) {
+console.log('F grade')
+}
+else {
+console.log('F grade')
+}
+// pete
+if (80 <= peteNum) {
+    console.log('A grade');
+}
+else if (60 <= peteNum) {
+    console.log('B grade')
+}
+else if (50 <= peteNum) {
+console.log('C grade')
+}
+else if (40 <= peteNum) {
+console.log('D grade')
+}
+else if (39 >= peteNum) {
+console.log('F grade')
+}
+else {
+console.log('F grade')
+}
+// jhon
+if (80 <= johnNum) {
+    console.log('A grade');
+}
+else if (60 <= johnNum) {
+    console.log('B grade')
+}
+else if (50 <= johnNum) {
+console.log('C grade')
+}
+else if (40 <= johnNum) {
+console.log('D grade')
+}
+else if (39 >= johnNum) {
+console.log('F grade')
+}
+else {
+console.log('F grade')
 }
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(myNum >= 80 && janeNum >= 80) {
+//     console.log('A+')
+// }
+// if(tomNum >= 60 ) {
+//     console.log('B')
+// }
+// else {
+//     console.log('C')
+// }
+// if(peteNum >= 50) {
+//     console.log('C')
+// }
+// if(johnNum >= 40) {
+//     console.log('D')
+// }
+
+
+// আপনি প্রত্যক এর নাম্বার কে চেক করবেন যে কন্ডিশন গুলো দেয়া হয়েছে সেগুলোর সাথে মিল করে । প্রথমে myNum কে চেক করবেন সব গুলো কন্ডিশন এর সাথে, এরপর সেম ভাবে বাকি দের গুলো ও চেক করতে হবে
 
 
