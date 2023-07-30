9cd63be120e7b661d6bf24d3f729fcd346e25860
shakibnymr <shakibnymr11@gmail.com>
2023-07-31 01:55:01 +0600

    pt2 completed

---
+++ pt2.js
@@ -0,0 +1,25 @@
+var myNum = 85;
+var tomNum = 66;
+var janeNum = 95;
+var peteNum = 56;
+var johnNum = 40;
+if(myNum >= 80 && janeNum >= 80) {
+    console.log('A+')
+}
+if(tomNum >= 60 ) {
+    console.log('B')
+}
+else {
+    console.log('C')
+}
+if(peteNum >= 50) {
+    console.log('C')
+}
+if(johnNum >= 40) {
+    console.log('D')
+}
+
+
+
+
+
