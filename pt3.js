4c0dc7daed2a24b388c4bc26fbc51eca6de3d311
shakibnymr <shakibnymr11@gmail.com>
2023-07-31 02:20:19 +0600

    pt3 completed

---
+++ pt3.js
@@ -0,0 +1,13 @@
+var numbers = [13, 79, 45]
+if (numbers[0] > numbers[1] || numbers[0] > numbers[2]) {
+    console.log('largest number')
+}
+else {
+    console.log('not found')
+}
+if (numbers[1] > numbers[2] || numbers[1] > numbers[0]) {
+    console.log('largest number')
+}
+else {
+    console.log('not found')
+}
